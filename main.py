7739d7555060dd2efcb65edf2294d07925263706
ANAND MOHAN SINGH <anandmohan.singh101@gmail.com>
2022-12-27 23:10:28 +0530

    fixed errors

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -129,14 +125,5 @@ def covid19prediction():
     plt.show()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
     covid19prediction()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
